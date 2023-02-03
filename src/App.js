4fc901aa99cbb33e51bc29c27101462638504aca
lynnlo <tharlynnhtet.ong@gmail.com>
2023-02-02 22:29:57 -0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import React from 'react';
+import React from 'react'
+import { Admin, Resource, ListGuesser } from 'react-admin'
+import jsonServerProvider from 'ra-data-json-server'
+
+const localDataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com')
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Admin dataProvider={localDataProvider}>
+      <Resource name="users" list={ListGuesser} />
+    </Admin>
+  )
 }
 
-export default App;
+export default App
