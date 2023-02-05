4b10fe6e71f11cc5728ffe124e029465766df883
lynnlo <tharlynnhtet.ong@gmail.com>
2023-02-05 12:18:11 -0700

    User Edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
 import React, { useState } from 'react'
 import {
   Admin,
+  ArrayInput,
   BooleanField,
+  BooleanInput,
   Create,
   Datagrid,
-  Form,
+  Edit,
   FunctionField,
   List,
   Resource,
   SimpleForm,
+  SimpleFormIterator,
   TextField,
   TextInput,
   Toolbar,
-  TopToolbar
 } from 'react-admin'
 
-import { Button, Card, CardContent, CircularProgress, Container, Typography } from '@mui/material'
+import { Button, Card, CardContent, CircularProgress, Container, Grid, Typography } from '@mui/material'
 import RefreshIcon from '@mui/icons-material/Refresh'
 import DeleteIcon from '@mui/icons-material/Delete'
 
 import localStorage from 'ra-data-local-storage'
 
+//#region Data Provider
 const localDataProvider = localStorage({
   defaultData: {
     users: [
@@ -276,6 +279,7 @@ const localDataProvider = localStorage({
     ],
   },
 })
+//#endregion
 
 //#region Dashboard
 let assignTargets = () => {
@@ -348,10 +352,11 @@ let clearHistory = () => {
 let Dashboard = () => {
   let [totalParticipants, setTotalParticipants] = useState(-1)
   let [totalEliminations, setTotalEliminations] = useState(-1)
+  let [totalBoughtBack, setTotalBoughtBack] = useState(-1)
   let [totalTeams, setTotalTeams] = useState(-1)
 
   localDataProvider.getList('users', { filter: { alive: true }, sort: { field: "name", order: "ascending" }, pagination: { page: 1 , perPage: 200 } } ).then(data => {
-    setTimeout(() => {setTotalParticipants(data.total)}, 500)
+    setTimeout(() => {setTotalParticipants(data.total)}, 300)
   })
 
   localDataProvider.getList('users', { sort: { field: "name", order: "ascending" }, pagination: { page: 1 , perPage: 200 } } ).then(data => {
@@ -360,7 +365,7 @@ let Dashboard = () => {
     users.forEach(user => {
       eliminations += user.eliminations.length
     })
-    setTimeout(() => {setTotalEliminations(eliminations)}, 500)
+    setTimeout(() => {setTotalEliminations(eliminations)}, 800)
   })
 
   localDataProvider.getList('users', { filter: { alive: true }, sort: { field: "name", order: "ascending" }, pagination: { page: 1 , perPage: 200 } } ).then(data => {
@@ -373,33 +378,47 @@ let Dashboard = () => {
         teams[user.team] = [user]
       }
     })
-    setTimeout(() => {setTotalTeams(Object.keys(teams).length)}, 500)
+    setTimeout(() => {setTotalTeams(Object.keys(teams).length)}, 1000)
+  })
+
+  localDataProvider.getList('users', { filter: { boughtBack: true }, sort: { field: "name", order: "ascending" }, pagination: { page: 1 , perPage: 200 } } ).then(data => {
+    setTimeout(() => {setTotalBoughtBack(data.total)}, 500)
   })
 
   return (
     <Card style={{margin: '1em'}}>
       <CardContent>
         <Container>
-        <Typography variant='h2' sx={ { m: 2 } }> Dashboard </Typography>
-        
-        <br />
+          <Typography variant='h2' sx={ { m: 2 } }> Dashboard </Typography>
+          <br />
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <Typography variant='h6' sx={ { m: 2 } }> Total Alive Participants: </Typography>
+              {
+                (totalParticipants === -1) ? <CircularProgress /> : <Typography variant='h1' sx={ { m: 2 } }> {totalParticipants} </Typography>
+              }
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Typography variant='h6' sx={ { m: 2 } }> Total Eliminations: </Typography>
+              {
+                (totalEliminations === -1) ? <CircularProgress /> : <Typography variant='h1' sx={ { m: 2 } }> {totalEliminations} </Typography>
+              }
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Typography variant='h6' sx={ { m: 2 } }> Total Alive Teams: </Typography>
+              {
+                (totalTeams === -1) ? <CircularProgress /> : <Typography variant='h1' sx={ { m: 2 } }> {totalTeams} </Typography>
+              }
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Typography variant='h6' sx={ { m: 2 } }> Total Amount Raised: </Typography>
+              {
+                (totalParticipants === -1 || totalBoughtBack === -1) ? <CircularProgress /> : <Typography variant='h1' sx={ { m: 2 } }> ${10 * (totalParticipants + totalBoughtBack)} </Typography>
+              }
+            </Grid>
+          </Grid>
 
-        <Typography variant='h6' sx={ { m: 2 } }> Total Alive Participants: </Typography>
-        {
-          totalParticipants === -1 ? <CircularProgress /> : <Typography variant='h1' sx={ { m: 2 } }> {totalParticipants} </Typography>
-        }
-
-        <Typography variant='h6' sx={ { m: 2 } }> Total Eliminations: </Typography>
-        {
-          totalParticipants === -1 ? <CircularProgress /> : <Typography variant='h1' sx={ { m: 2 } }> {totalEliminations} </Typography>
-        }
-
-        <Typography variant='h6' sx={ { m: 2 } }> Total Alive Teams: </Typography>
-        {
-          totalParticipants === -1 ? <CircularProgress /> : <Typography variant='h1' sx={ { m: 2 } }> {totalTeams} </Typography>
-        }
-
-
+          
         </Container>
 
         <Container>
@@ -417,7 +436,15 @@ let Users = () => {
   return (
     <List>
       <Datagrid>
-        <TextField source="name" />
+        <FunctionField label="Name" render={
+          record => (
+            <Button color={record.alive ? "success" : "error"} onClick={() => {
+              window.location.href = "/#/users/" + record.id
+            }}>
+              {record.name}
+            </Button>
+          )
+        } />
         <TextField source="team" />
         <TextField source="target" />
         <BooleanField source="alive" />
@@ -511,13 +538,42 @@ let UsersCreate = () => {
   )
 }
 
-//#endregion
+let UsersEdit = () => {
+  let [team, setTeam] = useState("")
 
+  let [name, setName] = useState("")
+
+  let EditActions = () => (
+    <Toolbar>
+      <Button disabled={(team === "" && name === "")} variant='contained' sx={{ m: 2 }} color="success" onClick={e => {createUser(e); window.location.href = "/#/users" }}> Save and Return </Button>
+      <Button disabled={(team === "" && name === "")} variant='outline' sx={{ m: 2 }} onClick={e => {createUser(e); window.location.reload(false) }}> Save  </Button>
+      <Button variant='outline' sx={{ m: 2 }} onClick={ () => { window.location.href = "/#/users" } }> Cancel </Button>
+    </Toolbar>
+  )
+
+  return (
+    <Edit>
+      <SimpleForm toolbar={<EditActions />}>
+        <TextInput source="name" onChange={e => { setName(e.target.value) }} />
+        <TextInput source="team" onChange={e => { setTeam(e.target.value) }} />
+        <TextInput source="target" />
+        <BooleanInput source="alive" />
+        <BooleanInput source="boughtBack" />
+        <ArrayInput source="eliminations" disabled>
+          <SimpleFormIterator disabled>
+            <TextInput disabled />
+          </SimpleFormIterator>
+        </ArrayInput>
+      </SimpleForm>
+    </Edit>
+  )
+}
+//#endregion
 
 function App() {
   return (
     <Admin dataProvider={localDataProvider} dashboard={Dashboard} title="Data Manager">
-      <Resource name="users" list={Users} create={UsersCreate} />
+      <Resource name="users" list={Users} create={UsersCreate} edit={UsersEdit} />
     </Admin>
   )
 }
